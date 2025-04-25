015d871b1835f19e57b5153e2d3bff103fdd0bc7
Stanislav-Popov <it.is.the.pochta@gmail.com>
2025-04-26 01:26:18 +0400

    frontend4

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -40,41 +40,44 @@ export default function AppHeader() {
             <Button type="primary" onClick={() => setDrawer(true)}>
                 Меню
             </Button>
+
             <Drawer title="Basic Drawer" onClose={() => setDrawer(false)} open={drawer}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                <Button type="text" block>
+                    text
+                </Button>
             </Drawer>
+
             {showCamera && (
-            <div
-                style={{
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    backgroundColor: "rgba(0,0,0,0.9)",
-                    zIndex: 1000,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}>
-                <Webcam
-                    ref={webcamRef}
-                    audio={false}
-                    screenshotFormat="image/jpeg"
-                    style={{ maxWidth: "100%" }}
-                />
-                <div style={{ marginTop: 20 }}>
-                    <Button onClick={capturePhoto} type="primary">
-                        Сделать фото
-                    </Button>
-                    <Button onClick={() => setShowCamera(false)} style={{ marginLeft: 10 }}>
-                        Отмена
-                    </Button>
+                <div
+                    style={{
+                        position: "fixed",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        backgroundColor: "rgba(0,0,0,0.9)",
+                        zIndex: 1000,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}>
+                    <Webcam
+                        ref={webcamRef}
+                        audio={false}
+                        screenshotFormat="image/jpeg"
+                        style={{ maxWidth: "100%" }}
+                    />
+                    <div style={{ marginTop: 20 }}>
+                        <Button onClick={capturePhoto} type="primary">
+                            Сделать фото
+                        </Button>
+                        <Button onClick={() => setShowCamera(false)} style={{ marginLeft: 10 }}>
+                            Отмена
+                        </Button>
+                    </div>
                 </div>
-            </div>)}
+            )}
         </Layout.Header>
     )
 }
