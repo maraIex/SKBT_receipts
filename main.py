04862a99d6f4a668a8d5ab3dd400d4007e9e58f7
EstrNous <praduntsev@yandex.ru>
2025-04-25 19:47:51 +0400

    -start OCR

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import requests
 
-url = "https://proverkacheka.com/api/v1/check/get"
- #пример параметров формата запроса 2
-data = {
- "token": "32628.CIPPhHYjg8dOqz8ql",
- "qrraw": "t=20250425T1301&s=120.00&fn=7380440801193957&i=14357&fp=1584076925&n=1",
+# Данные из QR-кода
+qr_data = "t=20240101T1200&s=100.00&fn=9289000100&i=12345&fp=1234567890"
+
+# Парсим параметры
+params = dict(x.split('=') for x in qr_data.split('&'))
+fn = params['fn']
+i = params['i']
+fp = params['fp']
+t = params['t']  # формат: 20240101T1200 → 2024-01-01T12:00:00
+
+# Форматируем дату
+formatted_date = f"{t[:4]}-{t[4:6]}-{t[6:8]}T{t[9:11]}:{t[11:13]}:00"
+
+# Ваш токен ФНС
+API_TOKEN = "32628.CIPPhHYjg8dOqz8ql"
+
+# URL запроса
+url = f"https://api-fns.ru/api/v1/ofds//inns//fss/{fn}/operations/1/tickets/{i}"
+
+# Параметры запроса
+params = {
+    "fiscalSign": fp,
+    "date": formatted_date,
+    "key": API_TOKEN  # ваш API-ключ
 }
- #пример параметров формата запроса 3
-data = {"token": "32628.CIPPhHYjg8dOqz8ql", "qrurl": "https://domen.ru/image/qrimage.jpg"}
- #пример параметров формата запроса 4
-data = {"token": "32628.CIPPhHYjg8dOqz8ql"}
-r = requests.post(url, data=data)
-print(r.text)
+
+# Отправка GET-запроса
+response = requests.get(url, params=params)
+
+# Обработка ответа
+if response.status_code == 200:
+    print("Чек найден:", response.json())
+else:
+    print("Ошибка:", response.status_code, response.text)
