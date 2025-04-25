bf033fedac3593361ce52776407748edc5a7d946
Stanislav-Popov <it.is.the.pochta@gmail.com>
2025-04-25 21:08:11 +0400

    frontend2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @format */
+import { Layout } from "antd"
+
+const headerStyle = {
+    textAlign: "center",
+    color: "#fff",
+    height: 60,
+    paddingInline: 48,
+    lineHeight: "64px",
+    backgroundColor: "#4096ff",
+}
+const contentStyle = {
+    textAlign: "center",
+    minHeight: "calc(100vh - 120px)",
+    color: "#fff",
+    backgroundColor: "#0958d9",
+}
+const footerStyle = {
+    height: 60,
+    textAlign: "center",
+    color: "#fff",
+    backgroundColor: "#4096ff",
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Layout>
+                <Layout.Header style={headerStyle}>Header</Layout.Header>
+                <Layout.Content style={contentStyle}>Content</Layout.Content>
+                <Layout.Footer style={footerStyle}>Footer</Layout.Footer>
+            </Layout>
+        </>
+    )
 }
 
 export default App
